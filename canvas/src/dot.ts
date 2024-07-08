e0db61d8cc8a401901322ba90f69e7037f0524bb
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-09 00:21:49 +0300

    finished ig

--- canvas/src/dot.ts
+++ canvas/src/dot.ts
@@ -1,19 +1,36 @@
-
 export class Dot {
-    x: number;
-    y: number;
-    speed: number;
-    radious: number;
-    color: string;
-    angel: number;
-  
-    constructor(color: string, centerX:number,centerY:number) {
-      this.radious = Math.floor(Math.random() * 300);
-      this.angel = 1;
-      this.speed = (Math.random() * 2 + 3) / 100;
-      this.x = centerX + Math.cos(this.angel) * this.radious;
-      this.y = centerY + Math.sin(this.angel) * this.radious;
-      // this.radius=Math.sqrt(x-midwidth**2 + y-midheight**2)
-      this.color = color;
-    }
+  x: number;
+  y: number;
+  centerX:number
+  centerY:number
+  speed: number;
+  radious: number;
+  color: string;
+  size:number
+  angel: number;
+
+  constructor(color: string, centerX: number, centerY: number) {
+    this.radious = Math.floor(Math.random() * 450);
+    this.angel = 1;
+    this.centerX = centerX
+    this.centerY = centerY;
+    this.size = Math.floor(Math.random() * 12)+1;
+    this.speed = (Math.random() * 3 + 1) / 100;
+    this.x = centerX + Math.cos(this.angel) * this.radious;
+    this.y = centerY + Math.sin(this.angel) * this.radious;
+    // this.radius=Math.sqrt(x-midwidth**2 + y-midheight**2)
+    this.color = color;
+  }
+  draw(ctx: CanvasRenderingContext2D) {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
+  }
+  update() {
+    this.angel += this.speed;
+    this.x = this.centerX + Math.cos(this.angel) * this.radious;
+    this.y = this.centerY + Math.sin(this.angel) * this.radious;
+    this.radious -= 0.2;
+  }
 }
--- canvas/src/main.ts
+++ canvas/src/main.ts
@@ -1,69 +1,55 @@
 import "./style.css";
-import {initalize} from './initializer.ts'
+import { initalize } from "./initializer.ts";
+import { Dot } from "./dot.ts";
 
-
-let ctx = initalize()
+let ctx = initalize();
 const img = new Image(); // Create new img element
 img.src = "public/black.png"; // Set source path
 
 let midheight = window.innerHeight / 2;
 let midwidth = window.innerWidth / 2;
 
-class Dot {
-  x: number;
-  y: number;
-  speed: number;
-  radious: number;
-  color: string;
-  angel: number;
-
-  constructor(color: string) {
-    this.radious = Math.floor(Math.random() * 300);
-    this.angel = 1;
-    this.speed = (Math.random() * 2 + 3) / 100;
-    this.x = midwidth + Math.cos(this.angel) * this.radious;
-    this.y = midheight + Math.sin(this.angel) * this.radious;
-    // this.radius=Math.sqrt(x-midwidth**2 + y-midheight**2)
-    this.color = color;
-  }
-  draw() {
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
-    ctx.fillStyle = this.color;
-    ctx.fill();
-  }
-  update() {
-    this.angel += this.speed;
-    this.x = midwidth + Math.cos(this.angel) * this.radious;
-    this.y = midheight + Math.sin(this.angel) * this.radious;
-    this.radious-=.4
-  }
-}
-
 let dots: Dot[] = [];
 
-function init(n:number) {
+function init(n: number) {
   for (let i = 0; i < n; i++) {
-    dots.push(new Dot("hsl(" + Math.random() * 360 + ", 100%, 50%)"));
+    dots.push(
+      new Dot(
+        "hsl(" + Math.random() * 360 + ", 100%, 50%)",
+        midwidth,
+        midheight
+      )
+    );
   }
 }
 
 init(10);
 
-function animate() {
-  ctx.fillStyle = "rgba(0,0,0,1)"; //black background
-  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
-  dots.forEach((dot,i) => {
-    dot.update();
-    dot.draw();
-    if(dot.radious<2){
-      dots.splice(i, 1);
+let angel=0
+ function animateimg(){
+  ctx.save()
+  ctx.translate(midwidth, midheight);
+  ctx.rotate(angel);
+  angel+=0.005
+  ctx.drawImage(img,-100,-100,200,200)
+  ctx.restore()
 
-        //add a new dot when the old one is gone
+ }
+function animate() {
+  ctx.fillStyle = "rgba(0,0,0,0.1)"; //black background
+  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  animateimg()
+  dots.forEach((dot, i) => {
+    dot.draw(ctx);
+    dot.update();
+
+    if (dot.radious < 2) {
+      dots.splice(i, 1);
     }
   });
   requestAnimationFrame(animate);
 }
 animate();
-
-
+// img.onload=()=>{
+//   ctx.drawImage(img,midwidth-150,midheight-150,300,300);
+// }
