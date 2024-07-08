52ef61d3ef11ecbb98d06c83f286f604c0bf900c
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-08 17:21:36 +0300

    added and loaded assets

--- canvas/src/main.ts
+++ canvas/src/main.ts
@@ -1,62 +1,77 @@
-import './style.css'
+import "./style.css";
 
-let canvas=document.querySelector('canvas') as HTMLCanvasElement;
-canvas.width =window.innerWidth
-canvas.height=window.innerHeight
-let ctx=canvas?.getContext('2d') as CanvasRenderingContext2D
+let canvas = document.querySelector("canvas") as HTMLCanvasElement;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+let ctx = canvas?.getContext("2d") as CanvasRenderingContext2D;
+const img = new Image(); // Create new img element
+img.src = "public/black.png"; // Set source path
 
-let midheight = window.innerHeight/2
-let midwidth = window.innerWidth/2
+let midheight = window.innerHeight / 2;
+let midwidth = window.innerWidth / 2;
 
-class Dot{
-  x:number
-  y:number
-  speed:number
-  radious:number
-  color:string
-  angel:number
+class Dot {
+  x: number;
+  y: number;
+  speed: number;
+  radious: number;
+  color: string;
+  angel: number;
 
-  constructor(color:string){
-    this.radious = Math.floor(Math.random()*300)
-    this.angel=1
-    this.speed=(Math.random()*2+3)/100 
-    this.x=midwidth + Math.cos(this.angel)*this.radious
-    this.y=midheight + Math.sin(this.angel)*this.radious
+  constructor(color: string) {
+    this.radious = Math.floor(Math.random() * 300);
+    this.angel = 1;
+    this.speed = (Math.random() * 2 + 3) / 100;
+    this.x = midwidth + Math.cos(this.angel) * this.radious;
+    this.y = midheight + Math.sin(this.angel) * this.radious;
     // this.radius=Math.sqrt(x-midwidth**2 + y-midheight**2)
-    this.color=color
-
+    this.color = color;
   }
-  draw(){
-    ctx.beginPath()
-    ctx.arc( this.x, this.y, 2, 0, Math.PI*2)
-    ctx.fillStyle=this.color
-    ctx.fill()
+  draw() {
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, 2, 0, Math.PI * 2);
+    ctx.fillStyle = this.color;
+    ctx.fill();
   }
-  update(){
-    this.angel+=this.speed
-    this.x = midwidth + Math.cos(this.angel)*this.radious
-    this.y = midheight + Math.sin(this.angel)*this.radious
-    // this.radious-=1
+  update() {
+    this.angel += this.speed;
+    this.x = midwidth + Math.cos(this.angel) * this.radious;
+    this.y = midheight + Math.sin(this.angel) * this.radious;
+    this.radious-=.4
   }
-
 }
 
-let dots :Dot[] = []
-for(let i=0; i<10; i++){
-  dots.push(new Dot( 'hsl(' + Math.random()*360 + ', 100%, 50%)'))
+let dots: Dot[] = [];
+
+function init(n:number) {
+  for (let i = 0; i < n; i++) {
+    dots.push(new Dot("hsl(" + Math.random() * 360 + ", 100%, 50%)"));
+  }
 }
 
+init(10);
 
+function animate() {
+  ctx.fillStyle = "rgba(0,0,0,1)"; //black background
+  ctx.fillRect(0, 0, window.innerWidth, window.innerHeight);
+  dots.forEach((dot,i) => {
+    dot.update();
+    dot.draw();
+    if(dot.radious<2){
+      dots.splice(i, 1);
 
-
-
-function animate(){
-  ctx.fillStyle = 'rgba(0,0,0,0.1)'  //black background
-  ctx.fillRect(0,0,window.innerWidth,window.innerHeight)
-  dots.forEach(dot => {
-    dot.update()
-    dot.draw()
-  })
-  requestAnimationFrame(animate)
+        //add a new dot when the old one is gone
+    }
+  });
+  requestAnimationFrame(animate);
 }
-animate()
+// animate();
+img.onload=()=>{
+  
+  ctx.drawImage(img,midwidth,midheight,50,50)
+}
+
+// function animateimg(){
+//   ctx.save()
+//   ctx.trans
+// }
