e2423d5fbf25b965a9a295e891fb1db148a7f6e3
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-08 09:57:51 +0300

    basic functionality

--- canvas/src/main.ts
+++ canvas/src/main.ts
@@ -1,24 +1,62 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+let canvas=document.querySelector('canvas') as HTMLCanvasElement;
+canvas.width =window.innerWidth
+canvas.height=window.innerHeight
+let ctx=canvas?.getContext('2d') as CanvasRenderingContext2D
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+let midheight = window.innerHeight/2
+let midwidth = window.innerWidth/2
+
+class Dot{
+  x:number
+  y:number
+  speed:number
+  radious:number
+  color:string
+  angel:number
+
+  constructor(color:string){
+    this.radious = Math.floor(Math.random()*300)
+    this.angel=1
+    this.speed=(Math.random()*2+3)/100 
+    this.x=midwidth + Math.cos(this.angel)*this.radious
+    this.y=midheight + Math.sin(this.angel)*this.radious
+    // this.radius=Math.sqrt(x-midwidth**2 + y-midheight**2)
+    this.color=color
+
+  }
+  draw(){
+    ctx.beginPath()
+    ctx.arc( this.x, this.y, 2, 0, Math.PI*2)
+    ctx.fillStyle=this.color
+    ctx.fill()
+  }
+  update(){
+    this.angel+=this.speed
+    this.x = midwidth + Math.cos(this.angel)*this.radious
+    this.y = midheight + Math.sin(this.angel)*this.radious
+    // this.radious-=1
+  }
+
+}
+
+let dots :Dot[] = []
+for(let i=0; i<10; i++){
+  dots.push(new Dot( 'hsl(' + Math.random()*360 + ', 100%, 50%)'))
+}
+
+
+
+
+
+function animate(){
+  ctx.fillStyle = 'rgba(0,0,0,0.1)'  //black background
+  ctx.fillRect(0,0,window.innerWidth,window.innerHeight)
+  dots.forEach(dot => {
+    dot.update()
+    dot.draw()
+  })
+  requestAnimationFrame(animate)
+}
+animate()
