a04270ff746bd23d013fef93aa194e01dc0ce748
HossamHamdy10241 <hossamhamdy10241@gmail.com>
2024-07-09 18:58:20 +0300

    nth

--- canvas/src/car.ts
+++ canvas/src/car.ts
@@ -5,14 +5,16 @@ canvas.width =window.innerWidth;
 canvas.height =window.innerHeight;
 let ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 
-
 let car={
     x:200,
     y:200,
     size:20,
-    color:'red'
+    dx:0,
+    color:'red',
+    angel:0
 }
 
+ctx.translate(car.x, car.y);
 
 const keys = {
     up: false,
@@ -36,13 +38,39 @@ const keys = {
   });
   
 function drawCar(){
-    ctx.fillStyle=car.color;
-    ctx.fillRect(car.x,car.y,car.size,car.size);
+   
+   
+   ctx.translate(car.dx,0)
+   ctx.rotate(car.angel);
+   ctx.fillStyle = car.color;
+   ctx.fillRect(-car.size/2, -car.size/2, car.size, car.size);
+}
+function update(){
+    if(keys.up){
+        car.dx=2
+
+    }
+    else if(keys.down){
+        car.dx=-2
+    }
+    else {car.dx=0}
+   
+    if(keys.left){
+        car.angel=-0.08
+    }
+    else if(keys.right){
+        car.angel=0.08
+    }
+    else{car.angel=0}
+
 }
 
 function animate(){
-    ctx.clearRect(0,0, canvas.width, canvas.height);
+    ctx.fillStyle='rgba(0,0,0,.1)'
+    ctx.fillRect(-canvas.width,-canvas.height, 2*canvas.width, 2*canvas.height);
+    update();
     drawCar();
+    // car.angel+=0.05
     requestAnimationFrame(animate);
 }
 animate()
